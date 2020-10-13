f1a96f9b2ff5f5ec662db40d345dd1f0ec983c92
Clemens Tolboom <clemens@build2be.com>
2020-10-13 21:02:16 +0200

    Tweaks to flying enemies.

--- models/Enemies/FlyingEnemy.gd
+++ models/Enemies/FlyingEnemy.gd
@@ -2,6 +2,9 @@ extends RigidBody
 
 var speed = 10
 
+export var max_flap:int = 10
+var flap:int = 0
+
 onready var player = get_node("/root/World/Player")
 
 # Enemy is attracted to player
@@ -11,3 +14,10 @@ func _physics_process(delta):
 
 	var dir = (him - me).normalized() * speed
 	add_force(dir, Vector3.ZERO)
+	if flap > 0:
+		flap -= 1
+		add_force(Vector3.UP * 25, Vector3.ZERO)
+
+
+func _on_Flap_timeout():
+	flap = max_flap
