2eae8df74a3eab04e16dc146ab201a34f5abc7e0
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-10-14 21:33:37 +0200

    Added functions for gaining  and losing ammo and health

--- src/HUD.gd
+++ src/HUD.gd
@@ -5,8 +5,11 @@ var ammo: int = 4;
 
 var status: Label;
 
-func set_heath(value):
-	health = value
+func set_heath(hp_value):
+	health = hp_value
+	
+func set_ammo(ammo_value):
+	ammo = ammo_value
 
 func _ready():
 	status = $Status/Label
