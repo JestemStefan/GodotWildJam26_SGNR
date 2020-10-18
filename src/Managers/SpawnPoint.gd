8904ab6f377f4304e49ad7927f78faf9441452f7
Clemens Tolboom <clemens@build2be.com>
2020-10-18 14:23:07 +0200

    Make spawnpoints have list of waves config.

--- src/Managers/SpawnPoint.gd
+++ src/Managers/SpawnPoint.gd
@@ -3,17 +3,42 @@ extends Spatial
 class_name SpawnPoint
 
 export (int) var percentage = 100
-export (int) var capacity = 50
-export (int) var wave_size = 5
+
+export (Array,int) var capacity = [50]
+export (Array, int) var wave_size = [5]
 
 export(Array,PackedScene) var enemies = []
 
+
 onready var nav: Navigation = get_tree().get_nodes_in_group("navigation")[0]
 
+
 var enemies_left: int
+var wave_index = 0
+
+
+func get_number_of_waves() -> int:
+	var result = -1
+	if capacity.size() == wave_size.size():
+		result = capacity.size()
+	return result
+
+
+func get_capacity() -> int:
+	return capacity[wave_index]
+
+
+func get_wave_size() -> int:
+	return wave_size[wave_index]
+
+
+func start_wave(n: int):
+	wave_index = n
+	enemies_left = get_capacity()
+
 
 func _ready():
-	enemies_left = capacity
+	start_wave(0)
 
 
 func is_depleted() -> bool:
@@ -28,7 +53,7 @@ func spawn_enemies():
 
 	#print_debug("New wave coming ...")
 	
-	for _i in range(wave_size):
+	for _i in range(get_wave_size()):
 		if enemies_left <= 0:
 			#print_debug("No more enemies left")
 			break
--- src/Managers/SpawnPointManager.gd
+++ src/Managers/SpawnPointManager.gd
@@ -3,8 +3,10 @@ extends Spatial
 onready var timer = $Timer
 
 export var every_seconds: float = 5.0
+export var number_of_waves: int = 2
 
 var spawn_points: Array = []
+var wave_index = 0
 
 # We use percentages to random select SpawnPoint
 # Say: a = 150% b = 75% c = 25%
@@ -16,18 +18,42 @@ var total_percentages:int = 0
 
 var rng = RandomNumberGenerator.new()
 
+func start_wave():
+	if (wave_index < number_of_waves):
+		for c in get_children():
+			if c is SpawnPoint:
+				var s:SpawnPoint = c
+				s.start_wave(wave_index)
+		wave_index += 1
+
+
 func _ready():
 	rng.randomize()
 
 	timer.wait_time = every_seconds
 
+	var spawn_points_number_of_waves = 0
 	for c in get_children():
 		if c is SpawnPoint:
 			var s:SpawnPoint = c
+			var num_waves = s.get_number_of_waves()
+			if num_waves > 0:
+				if spawn_points_number_of_waves == 0:
+					spawn_points_number_of_waves = num_waves
+				elif spawn_points_number_of_waves == num_waves:
+					pass
+				else:
+					spawn_points_number_of_waves = -1
 			if s.visible:
 				spawn_points.push_back(s)
 				percentages.push_back(s.percentage)
 				total_percentages += s.percentage
+
+	if spawn_points_number_of_waves == -1:
+		print_debug("Imbalanced number of waves: check Spawn Manager and Points")
+		get_tree().quit()
+
+	start_wave()
 
 func _on_Timer_timeout():
 	# We select one SpawnPoint based on its percentage
